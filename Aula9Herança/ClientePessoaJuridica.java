46c8a41b136fe87e27d549b2ca31abcd8399294c
Ilson Warmling <77305928+ilsonwar@users.noreply.github.com>
2021-10-26 20:38:25 -0300

    Add files via upload

---
+++ Aula9Herança/ClientePessoaJuridica.java
@@ -0,0 +1,64 @@
+package net.Unibave.Aula9Herança;
+
+import java.time.LocalDateTime;
+
+public class ClientePessoaJuridica extends Cliente {
+
+    private String razaoSocial;
+    private String fantasia;
+    private String cnpj;
+    private LocalDateTime dataRegistro;
+
+    public ClientePessoaJuridica() {
+
+    }
+    public ClientePessoaJuridica(String razaoSocial, String fantasia, String cnpj, LocalDateTime dataRegistro) {
+        this.razaoSocial = razaoSocial;
+        this.fantasia = fantasia;
+        this.cnpj = cnpj;
+        this.dataRegistro = dataRegistro;
+    }
+
+
+    public String getRazaoSocial() {
+        return razaoSocial;
+    }
+
+    public void setRazaoSocial(String razaoSocial) {
+        this.razaoSocial = razaoSocial;
+    }
+
+    public String getFantasia() {
+        return fantasia;
+    }
+
+    public void setFantasia(String fantasia) {
+        this.fantasia = fantasia;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public LocalDateTime getDataRegistro() {
+        return dataRegistro;
+    }
+
+    public void setDataRegistro(LocalDateTime dataRegistro) {
+        this.dataRegistro = dataRegistro;
+    }
+
+    @Override
+    public String toString() {
+        return "ClientePessoaJuridica{" +
+                "razaoSocial='" + razaoSocial + '\'' +
+                ", fantasia='" + fantasia + '\'' +
+                ", cnpj='" + cnpj + '\'' +
+                ", dataRegistro=" + dataRegistro +
+                '}';
+    }
+}
